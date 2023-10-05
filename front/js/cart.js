a69a4d76b8777d9dc520f2689406cc451bec707e
EvolutionZ <139359712+EvolutionZ19@users.noreply.github.com>
2023-10-05 15:30:04 +0200

    Ajout de la page cart et son .js

--- front/js/cart.js
+++ front/js/cart.js
@@ -1,8 +1,53 @@
-
-
-
 let panier = JSON.parse(localStorage.getItem("panier"));
-console.log(panier);
+
+// faire parse, foreach pour afficher les element du panier, insert html et le remove si suprimmer dans le panier et le local storage
+
+
+
+
+function afficherPanier() {
+    const produitPanier = JSON.parse(localStorage.getItem("panier"));
+    const panier = document.querySelector(".panier");
+
+    if(!produitPanier || produitPanier.length === 0) {
+        panier.insertAdjacentHTML = `
+        <div class="panier">
+            <p>Votre panier est vide</p>
+        </div>
+        `;
+        return
+    }
+    // const id = produitPanier.map(el => el.id);
+    // async function fetch(id) {
+    //     const req = await fetch(`http://localhost:3000/api/products/${id}`);
+    //     const data = await req.json();
+    //     return data;
+    // }
+
+    produitPanier.forEach(async (el, index) => {
+        let produit = await fetch(`http://localhost:3000/api/products/${el.id}`);
+        produit = await produit.json()
+        const panier = document.querySelector(".panier");
+        panier.innerHTML += `
+            <div class="ProduitsPanier">
+                <p>${el.image}</p>
+                <p>${el.titre}</p>
+                <p>${el.prix}</p>
+                <p>${el.quantite}</p>
+                <p>${el.format}</p>
+                <button class="supprimer" onclick="supprimer(${index})">Supprimer</button>
+            </div>
+        `;
+    });
+
+
+    
+}
+
+
+afficherPanier();
+
+
 
 
 
